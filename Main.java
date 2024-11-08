11fcb4b38d07c10b1c6134cd3495c0fe5cc3f101
George Novachuk <george@Georges-MacBook-Pro.local>
2024-11-08 03:22:21 +0000

    fix bluej integration. Now works with BlueJ

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 import game.Game;
 import game.Room;
 import game.factories.RoomFactory;
-import jsonParser.JSONObject;
 import jsonParser.JSONParser;
 import persistence.GameContentFile;
 import persistence.JsonReader;
 
 public class Main {
     public static void main(String[] args) {
+        new Main().initializeGame();
+    }
+    
+    public void initializeGame() {
         for (GameContentFile roomFile : new JsonReader().readGameDataFiles("rooms")) {
             RoomFactory.createRoom(new JSONParser(roomFile.getContents()).parse());
         }
